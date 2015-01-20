d9ef63410a986cd8185921eef2922186afb9d240
Michael Germany <mike@rickgermany.com>
2015-01-20 10:42:22 -0700

    final draft

--- main.js
+++ main.js
@@ -3,10 +3,42 @@
 var phoneNumber = prompt("Please enter your phone number with dashes.\n" + 
 	"(It won't be used for SPAM.)");
 
-if (phoneNumber.charAt(4) === "-" && phoneNumber.charAt(8) === "-") {
-	alert (true);
+if (phoneNumber.charAt(3) === "-" && phoneNumber.charAt(7) === "-") {
+	alert(true);
 } else {
-	alert (false);
-}
+	alert(false);
 }
 
+var birthDate = prompt("Please enter your birthdate (xx/xx/xx)");
+
+if (birthDate.charAt(2) === "/" && birthDate.charAt(5) === "/") {
+	alert(true);
+} else {
+	alert(false);
+}
+
+var postalCode = prompt("Please enter your postal code (xxxxx)");
+
+if (postalCode.length === 5) {
+	alert(true);
+} else {
+	alert(false);
+}
+
+var state = prompt("Please enter your state abbreviation");
+
+if (state.length === 2 && state === state.toUpperCase()) {
+	alert(true);
+} else {
+	alert(false);
+}
+
+var maritalStatus = prompt("Please enter your marital status (yes or no)");
+
+maritalStatus = maritalStatus.toUpperCase();
+
+if (maritalStatus === "YES" || maritalStatus === "NO") {
+	alert(true);
+} else {
+	alert(false);
+}
